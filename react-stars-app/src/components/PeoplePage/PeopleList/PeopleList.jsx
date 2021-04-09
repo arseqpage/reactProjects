3bfdd5b3ec3d32ec6b57bd50f2e2c4a5a8bb1931
Arslan <arseqpage@gmail.com>
2021-04-09 23:25:07 +0300

    1 added 2 files

---
+++ react-stars-app/src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -0,0 +1,18 @@
+import styles from './PeopleList.module.css';
+
+const PeopleList = ({ people }) => {
+  return (
+    <ul className={styles.list__container}>
+      {people.map(({ id, name, img }) => (
+        <li className={styles.list__item} key={`${id}${name}`}>
+          <a href="#">
+            <img className={styles.person__photo} src={img} alt={name}></img>
+            <p>{name}</p>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PeopleList;
