f681f570bf518a91264cd73ebbe4ef979548b81f
Arslan <arseqpage@gmail.com>
2021-05-13 19:30:24 +0300

    Save items in locstor

--- react-stars-app/src/containers/FavoritePage/FavoritePage.jsx
+++ react-stars-app/src/containers/FavoritePage/FavoritePage.jsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
+import PeopleList from '@components/PeoplePage/PeopleList';
 
 import styles from './FavoritePage.module.css';
 
 const FavoritePage = () => {
+  const [people, setPeople] = useState([]);
+
   const storeData = useSelector((store) => store.favoriteReducer);
 
-  return <h1>FavoritePage</h1>;
+  useEffect(() => {
+    const arr = Object.entries(storeData);
+
+    if (arr.length) {
+      const res = arr.map((item) => {
+        return {
+          id: item[0],
+          ...item[1],
+        };
+      });
+
+      setPeople(res);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1 className="header__text">FavoritePage</h1>
+      {people.length ? <PeopleList people={people} /> : <h2 className={styles.comment}>No data</h2>}
+    </>
+  );
 };
 
 export default FavoritePage;
